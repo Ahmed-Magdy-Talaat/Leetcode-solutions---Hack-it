3d067252e86eff64cba02b358bc352fd26510a58
Ahmed-Magdy-Talaat <85371544+Ahmed-Magdy-Talaat@users.noreply.github.com>
2023-10-24 16:01:20 +0300

    Time: 44 ms (10.49%), Space: 12.7 MB (15.8%) - LeetHub

--- 0981-delete-columns-to-make-sorted/0981-delete-columns-to-make-sorted.cpp
+++ 0981-delete-columns-to-make-sorted/0981-delete-columns-to-make-sorted.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
-set<int>st;
+
     int minDeletionSize(vector<string>& strs) {
+        map<int,int>mp;
         for(int i=1;i<strs.size();i++){
             for(int j=0;j<strs[i].size();j++){
                 if(strs[i][j]<strs[i-1][j]){
-                    st.insert(j);
+                    mp[j]++;
                 }
             }
         }
-        return st.size();
+        return mp.size();
     }
 };
